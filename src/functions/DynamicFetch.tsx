e90b8209e9da58afb2ebc71f09b2acc1bbb5ef4c
Karan Anand <thekarananand@gmail.com>
2024-08-24 10:13:11 +0530

    feat: logic for HeaderTop Notifications

---
+++ src/functions/DynamicFetch.tsx
@@ -0,0 +1,15 @@
+const Repo = "https://raw.githubusercontent.com/E2A-Conference/e2a.dynamicData.json/main/";
+
+const DynamicFetch = async(file: string) => {
+    let response = await fetch( Repo + `${file}`, { cache: "no-store" } );
+        
+    if (response.ok) {
+        let data = await response.json();
+        return ( data );
+    }
+    else {
+        return ( { "error": "error" } );
+    }
+}
+
+export default DynamicFetch
