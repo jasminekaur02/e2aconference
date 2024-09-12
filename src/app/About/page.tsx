6ff2e2ec80173c2b809c3522cc3fa8846025767f
Jasmine Kaur <jasminekaurmukkar@gmail.com>
2024-09-12 17:45:54 +0530

    team

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -3,7 +3,7 @@ import Footer from '@/components/Footer';
 import HeaderTop from '@/components/HeaderTop';
 import Navbar from '@/components/navbar';
 import PageHero from '@/components/PageHero';
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import Link from 'next/link';
 
 const AboutUsPage = () => {
@@ -24,7 +24,7 @@ const AboutUsPage = () => {
           <div className="bg-white p-4 rounded-lg shadow-lg max-w-6xl mx-auto">
             <h2 className="text-4xl font-bold text-center mb-8 ">Who We <span className='text-blue-600'>Are</span></h2>
             <p className="text-lg leading-relaxed text-justify">
-              The International Conference on Electrical, Electronics, and Automation (E2ACON) serves as a global hub where the brightest minds converge to explore, collaborate, and share their ground-breaking research and innovations. In today's rapidly evolving technological landscape, the domains of electrical engineering, electronics, and automation are integral to shaping our future. E2A provides a vital platform for researchers, scientists, engineers, educators, and industry experts from diverse backgrounds and cultures to come together, fostering a rich exchange of ideas and knowledge.
+              The International Conference on Electrical, Electronics, and Automation (E2ACON) serves as a global hub where the brightest minds converge to explore, collaborate, and share their ground-breaking research and innovations. In today&apos;s rapidly evolving technological landscape, the domains of electrical engineering, electronics, and automation are integral to shaping our future. E2A provides a vital platform for researchers, scientists, engineers, educators, and industry experts from diverse backgrounds and cultures to come together, fostering a rich exchange of ideas and knowledge.
             </p>
             <p className="mt-6 text-lg leading-relaxed text-justify">
               As we navigate an era of unprecedented technological advancement, E2ACON strives to accelerate progress by facilitating meaningful conversations, driving innovation, and charting the course for a future powered by cutting-edge electrical, electronics, and automation technologies. Join us at E2ACON to be a part of this transformative journey, where together, we illuminate the path toward a smarter, more connected, and automated world. Your contributions and insights are integral to shaping the future at E2A.
@@ -39,8 +39,10 @@ const AboutUsPage = () => {
             <div className="bg-white p-8 rounded-lg shadow-lg">
               <h3 className="text-3xl font-bold mb-4">About <span className='text-blue-600'>NIT Jalandhar</span></h3>
               <p className='text-sm text-blue-600 p-2'>
-                 <Link href="https://www.nitj.ac.in" legacyBehavior>
-              <a href="https://www.nitj.ac.in" target="_blank">https://www.nitj.ac.in</a></Link></p>
+                <Link href="https://www.nitj.ac.in" legacyBehavior>
+                  <a href="https://www.nitj.ac.in" target="_blank">https://www.nitj.ac.in</a>
+                </Link>
+              </p>
               <p className="text-md leading-relaxed text-justify">
                 Dr. B. R. Ambedkar NIT Jalandhar was established in 1987 as an erstwhile Regional Engineering College and later given the status of NIT Jalandhar by the Ministry of Human Resource Development, Government of India in 2002.
                 {showMoreNit && (
@@ -58,8 +60,10 @@ const AboutUsPage = () => {
             <div className="bg-white p-8 rounded-lg shadow-lg">
               <h3 className="text-3xl font-bold mb-4">Department of <span className='text-blue-600'>ICE</span></h3>
               <p className='text-sm text-blue-600 p-2'>
-              <Link href="https://departments.nitj.ac.in/dept/ice/home" legacyBehavior>
-                <a href="https://departments.nitj.ac.in/dept/ice/home" target="_blank">https://departments.nitj.ac.in/dept/ice/home</a></Link></p>
+                <Link href="https://departments.nitj.ac.in/dept/ice/home" legacyBehavior>
+                  <a href="https://departments.nitj.ac.in/dept/ice/home" target="_blank">https://departments.nitj.ac.in/dept/ice/home</a>
+                </Link>
+              </p>
               <p className="text-md leading-relaxed text-justify">
                 The Department of Instrumentation and Control Engineering (ICE) commenced its Bachelor of Technology (B.Tech) degree program in 1990 with an initial intake of 30 students.
                 {showMoreIce && (
