4f965ca1b32d419fbd681bd0e5589265c325ceab
Ayush Kumar <ayushkr0104@hotmail.com>
2025-02-16 10:18:20 +0530

    speaker

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -72,6 +72,15 @@ const speakers: Speaker[] = [
       "/randolph_brazier.jpeg",
     linkedInUrl: "https://www.linkedin.com/in/randolph-brazier/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
   },
+  {
+    id: 7,
+    name: "Mr. Aditya Kushwah",
+    title: "Technical Project Manager | Zero-Emission Fleet Transition | Techno-Economic Modeling",
+    organization: "CALSTART, United States",
+    imageUrl: "/adi_speaker.jpg",
+    linkedInUrl: "https://www.linkedin.com/in/adityasing012194/"
+  },  
+
 ];
 
 const SpeakerCard: React.FC<Speaker> = ({
